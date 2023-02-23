cd6d4d2bbd04a243f37d721f3486b16802fbb883
kamabokochan <h.sasaki.worksfront@gmail.com>
2023-02-24 01:04:13 +0900

    styled-components追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import styled from 'styled-components'
 import { Layout } from '@/components/layout/MainLayout'
 
 export default function Home() {
   return (
     <Layout>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <Wrapper>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      </Wrapper>
     </Layout>
   )
 }
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`
