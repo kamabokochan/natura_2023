b3339bddbcbcbbd523c3032d97aca74a2f30504b
kamabokochan <h.sasaki.worksfront@gmail.com>
2023-02-23 20:57:45 +0900

    fix format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='ja'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
